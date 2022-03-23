f763aeb64cf6f13788686f1f4f87ec353040c063
troymeeker <troymeeker1@gmail.com>
2022-03-22 17:28:41 -0700

    minor styling

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,9 +5,15 @@ function About(){
         <div className="about">
             <h2>About</h2>
             <div className="text">
-                <p>This is an app that is designed to mimic the functionality of craigslist. A user can create and login to their account, create posts, read through current posts, update a post or delete a post. 
-                    A user can also add any post to their favorite list and subsequently remove that item on the Favorites page.
+                <p>
+                    This is an application that is designed to mimic the basic functionality of craigslist. 
+                    A user can create an account or login to their existing account, create posts, read through current posts, update a posts description and price or delete a post. 
+                    This application will be expanded upon soon!<br/>
+                    Thanks for visiting!
+
+                    {/* A user can also add any post to their favorite list and subsequently remove that item on the Favorites page. */}
                 </p>
+                
             </div>
         </div>
     )
